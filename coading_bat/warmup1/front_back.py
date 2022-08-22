9cf2a64470facdf7a1d0c02de45cbd6c171efbfa
alamin <74412051+md-alamin-hossain@users.noreply.github.com>
2022-08-22 07:24:40 +0600

    fixed

--- coading_bat/warmup1/front_back.py
+++ coading_bat/warmup1/front_back.py
@@ -8,14 +8,7 @@ front_back('ab') → 'ba'
 
 
 def front_back(str):
-	m_str = str[1:len(str) - 1]
-	f_str = str[:1]
-	l_str = str[len(str) - 1]
-	
-	if len(str) == 1:
-		return str
-	elif len(str) == 2:
-		return l_str + f_str
-	else:
-		return l_str + m_str + f_str
-		
+    if len(str) < 2:
+        return str
+    new_str = str[len(str) - 1] + str[1: len(str) - 1] + str[0]
+    return new_str
