9b7d2b98f7e6f867d5ff18b36d93c016db09ef20
alamin <74412051+md-alamin-hossain@users.noreply.github.com>
2022-08-22 19:28:09 +0600

    Create add_them_up.py

---
+++ toph/easy_problems/add_them_up.py
@@ -0,0 +1,19 @@
+"""
+encountered = সম্মুখীন
+
+Read two integer variables, calculate their sum, and print it.
+
+Input
+The input will contain two integers $A$ and $B$ ($-20000000 < A, B < 20000000$).
+
+Output
+Print the sum of the two integers.
+
+Sample
+Input	Output
+4 5
+9
+"""
+
+a, b = map(int, input().split())
+print(a + b)
