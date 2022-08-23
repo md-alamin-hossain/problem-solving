443280ad70110e279fe8350d41e2183ce60449aa
alamin <74412051+md-alamin-hossain@users.noreply.github.com>
2022-08-23 10:56:19 +0600

    Create string_times.py

---
+++ coading_bat/warmup_2/string_times.py
@@ -0,0 +1,17 @@
+"""
+Given a string and a non-negative int n, return a larger string that is n copies of the original string.
+
+
+string_times('Hi', 2) → 'HiHi'
+string_times('Hi', 3) → 'HiHiHi'
+string_times('Hi', 1) → 'Hi'
+"""
+
+
+def string_times(a, b):
+	return a * b
+	
+
+print(string_times("hi", 2))
+print(string_times("hi", 3))
+print(string_times("hi", 1))
